e3f073c6490bef3b55f4d39f44d46f6cb43dcc47
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-06-21 23:10:15 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-[#292524] h-screen w-screen">
+    
+     <div class="flex justify-center flex-col align-middle w-[300px] mx-auto">
+      <img src="./assets/Auto-check.png" class="max-w-[350px]" />
+      <input type="text" placeholder="Apelido do veículo" class="w-[300px] p-2 shadow-inner border-b-indigo-500 border-4">
+      <input type="text" placeholder="Placa" class="w-[100px] p-2 mt-2 shadow-inner border-b-indigo-500 border-4">
+      <button class="bg-sky-500 mt-5 rounded-br-lg">Começar</button>
+     </div>
+  </div>
+ 
+</template>
